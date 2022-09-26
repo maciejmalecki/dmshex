9534953fd61cad3cffee8208ab087bb9d7c38c4e
Maciej Malecki <malecki.maciej@gmail.com>
2022-09-26 07:25:41 +0200

    web controller

---
+++ preparation/adapters/in/web/src/main/java/dmshex/preparation/adapters/in/web/PreparationControllerExceptionHandler.java
@@ -0,0 +1,18 @@
+package dmshex.preparation.adapters.in.web;
+
+import dmshex.preparation.RevisionNotEditableException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class PreparationControllerExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(RevisionNotEditableException.class)
+    public ResponseEntity<Object> handleNotEditable(RuntimeException ex, WebRequest req) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE, req);
+    }
+}
---
+++ preparation/adapters/in/web/src/main/java/dmshex/preparation/adapters/in/web/RevisionRestController.java
@@ -0,0 +1,37 @@
+package dmshex.preparation.adapters.in.web;
+
+import dmshex.preparation.api.CreateRevisionCommand;
+import dmshex.preparation.api.RevisionQueryModel;
+import dmshex.preparation.usecase.CreateRevisionUseCase;
+import dmshex.preparation.usecase.QueryRevisionByIdUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class RevisionRestController {
+    private final CreateRevisionUseCase createRevisionUseCase;
+    private final QueryRevisionByIdUseCase queryRevisionByIdUseCase;
+
+    @PostMapping("/revisions")
+    public ResponseEntity<String> createRevision(@RequestBody CreateRevisionCommand command) {
+        final var revisionId = createRevisionUseCase.apply(command);
+        return new ResponseEntity<>(String.valueOf(revisionId), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/revisions/{revisionId}")
+    public RevisionQueryModel getRevision(@PathVariable Long revisionId) {
+        return Optional.ofNullable(revisionId)
+                .flatMap(queryRevisionByIdUseCase::queryBy)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
